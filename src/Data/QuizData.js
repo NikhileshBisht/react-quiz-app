9fe0c1382b9a2d11cc1e20898eeadb58c7ad5f04
Nikhilesh Bisht <nikkihotshot@gmail.com>
2024-03-25 21:01:29 +0530

    upload

---
+++ src/Data/QuizData.js
@@ -0,0 +1,22 @@
+export const QuizData = [
+    {
+        question: "How can we describe an array in the best possible way?",
+        options:[ "The Array shows a hierarchical structure.", "Arrays are immutable.", "Container that stores the elements of similar types", "The Array is not a data structure"],
+        answer: 3
+    },
+    {
+        question: "Which of the following is the correct way of declaring an array??",
+        options:["int javatpoint[10];", "int javatpoint;", "javatpoint{20};", "array javatpoint[10];"],
+        answer: 1
+    },
+    {
+        question: "Which of the following is the advantage of the array data structure?",
+        options: ["Elements of mixed data types can be stored.", "Easier to access the elements in an array", "Index of the first element starts from 1.", "Elements of an array cannot be sorted"],
+        answer: 2
+    },
+    {
+        question: "Which of the following highly uses the concept of an array?",
+        options: ["Binary Search tree", "Caching", "Spatial locality", "Scheduling of Processes"],
+        answer:3
+    },
+];
